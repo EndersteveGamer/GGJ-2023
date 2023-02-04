118caf56445101583916b157b6bda0515f7ab6a7
FlameofChange <lordredelapierre@gmail.com>
2023-02-04 16:02:59 +0100

    gimme de plant

--- The Other Godot Project/Script/Rat.gd
+++ The Other Godot Project/Script/Rat.gd
@@ -14,7 +14,7 @@ var uproot=null #the plant the rat picked, if null the rat is empty handed
 var uprooting=0
 export var timeToUproot=0.5
 
-var plantGray=preload()
+#var plantGray=preload('')
 
 var index=0
 
