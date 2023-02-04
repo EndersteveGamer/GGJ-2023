d59c02b4f378ba9333abcb1c859d41f6a4af8863
FlameofChange <lordredelapierre@gmail.com>
2023-02-04 11:08:28 +0100

    Grid again

--- The Other Godot Project/Script/Game.gd
+++ The Other Godot Project/Script/Game.gd
@@ -1,17 +1,41 @@
 extends Node2D
 
 var grid=[]
+var gridSize=64
 var gridWidth=64
-
+onready var plant=get_node("Plant")
 func pointToGrid(x):
-	return floor(x/gridWidth)*64
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+	return floor(x/gridWidth)*gridSize
 
+func stickToGrid(x):
+	gridGet(x).position.x=pointToGrid(gridGet(x).position.x)
+
+func gridGet(x):
+	return grid[x]
+
+func gridHas(x):
+	return gridGet(x)!=null
+
+func gridSet(x,what):
+	if not gridHas(x):
+		grid[x]=what
+
+func gridTake(x):
+	if gridHas(x):
+		var got=gridGet(x)
+		grid[x]=null
+		return got
+	return null
+
+func createPlant(x):
+	var newPlant=plant.instance()
+	add_child(newPlant)
+	gridSet(x,newPlant)
+	return newPlant
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	grid.resize(gridSize)
 	pass # Replace with function body.
 
 
