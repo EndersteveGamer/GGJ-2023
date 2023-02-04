f44d1fc44676185deeff8d07286fcf2ae1754793
FlameofChange <lordredelapierre@gmail.com>
2023-02-04 11:31:49 +0100

    Plant random spawn

--- The Other Godot Project/Script/Game.gd
+++ The Other Godot Project/Script/Game.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 var grid=[]
-var gridSize=64
-var gridWidth=64
-onready var plant=get_node("Plant")
+var gridSize=64 # number of case
+var gridWidth=64 # width of each case
+onready var plant=preload("res://Scene/Plant.tscn")
+onready var rng=RandomNumberGenerator.new()
+
 func pointToGrid(x):
-	return floor(x/gridWidth)*gridSize
+	return floor(x/gridWidth)*gridWidth
+	
+func gridToPoint(x):
+	return x*gridWidth
 
 func stickToGrid(x):
 	gridGet(x).position.x=pointToGrid(gridGet(x).position.x)
@@ -20,6 +25,7 @@ func gridSet(x,what):
 	if not gridHas(x):
 		grid[x]=what
 		what.index=x
+		what.position.x=gridToPoint(x)
 
 func gridTake(x):
 	if gridHas(x):
@@ -30,14 +36,30 @@ func gridTake(x):
 	return null
 
 func createPlant(x):
-	var newPlant=plant.instance()
-	add_child(newPlant)
-	gridSet(x,newPlant)
-	return newPlant
+	print(plant)
+	if plant!=null:
+		var newPlant=plant.instance()
+		add_child(newPlant)
+		gridSet(x,newPlant)
+		return newPlant
+	return null
+
+func spawnPlantRandom():
+	var x=rng.randi()%gridSize
+	while gridHas(x):
+		x+=1
+		if x>gridSize:
+			x=0
+	return createPlant(x)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	grid.resize(gridSize)
+	rng.randomize()
+	spawnPlantRandom()
+	spawnPlantRandom()
+	spawnPlantRandom()
+	spawnPlantRandom()
 	pass # Replace with function body.
 
 
